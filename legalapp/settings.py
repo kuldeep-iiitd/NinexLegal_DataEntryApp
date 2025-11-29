429e030b51e8ed45d73c9463bce00798651c7d2b
Kuldeep Singh <kumarmunesh0942@gmail.com>
2025-11-29 20:15:37 +0530

    ready for production

--- legalapp/settings.py
+++ legalapp/settings.py
@@ -10,8 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#7j8%^mi7s9q83uo%ab9+!6$%n_wq@*shm@=w-ah#l-b@lz9zs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 
 
